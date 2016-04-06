71464b475dc59a02c28caa4d7c15b68d86c12714
zbobos <zbobos@localhost>
2016-04-06 17:45:02 -0400

    comments

--- TestDogBLC/src/edu/saintjoe/cs/brianc/testdogblc/TestDogBLC.java
+++ TestDogBLC/src/edu/saintjoe/cs/brianc/testdogblc/TestDogBLC.java
@@ -1,33 +1,34 @@
 package edu.saintjoe.cs.brianc.testdogblc;
 
+// class is named TestDogBlc
 public class TestDogBLC {
-
+// represents the TestDogBlc's name, also a string object
 	    private String name;
-	    
+	    // is the TestDogs size, called weight 
 	    private int weight;
-	    	    
+	   // is an instance method for name 
 	    public void setName(String newName) {
 	    	name = newName;
 	    }
-	    
+	    // is an instance method for its size
 	    public void setWeight(int newWeight) {
 	    	weight = newWeight;
 	    }
 	    
-
+	    //calls the operation of its name from above and returns the value of the name
 	    public String getName() {
 	    	return name;
 	    }
-	    
+	    // calls the operation of its size from above and returns the value of its weight 
 	    public int getWeight() {
 	    	return weight;
 	    }
-	    
+	    // starts the local scope of bark()
 	    public void bark() {
-	    
+	    // have to give int a value 
 	    	int pointless = 0;
 	    	
-	    	
+	    	// tells the operation what to do and when to do it depending on the int
 	    	if (weight > 60 ) {
 	          System.out.println("Woof!! Woof!! says " + name + " that weighs " + weight);
 	    	} else if (weight > 14 ) {
@@ -35,7 +36,7 @@ public class TestDogBLC {
 	    	} else
 	    		System.out.println("Yip!! Yip!! says " + name + " that weighs " + weight);
 	    }
-	    
+	    // local variable
 	    public int setBoth(String newName, int newWeight) {
 	    	
 	    	int oldWeight = weight;
@@ -45,9 +46,9 @@ public class TestDogBLC {
 	    	return oldWeight;
 	    }
 	    	
-
+// make eat() public and has no return value
 	    public void eat() { }
-	    
+	    // make chaseCAt() public and has no return value
 	    public void chaseCat() { }
 	}
 
